419390b2c2afd33df9fcb2b04f97fb9abb65a5ab
neha <neha.lore@moonraft.com>
2022-11-15 10:37:29 +0530

    2nd commit

---
+++ lib/my_folder/home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        centerTitle: true,
+        title: const Text('Home Page'),
+      ),
+      body: const Center(child: Text('Flutter Demo Home Page')),
+    );
+  }
+}
